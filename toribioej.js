31321d73d12f8996434153425dd1cc3cba56219e
EnrickJason-Toribio <toribio.enrickjason@gmail.com>
2025-10-23 11:04:40 +0800

    Added toribioej.html, toribioej.css, toribioej.js

---
+++ toribioej.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const clickButton = document.getElementById("clickbutton");
+    clickButton.addEventListener("click", function(){
+        alert("Hello! I am Enrick Jason I. Toribio, 22 years of age, residing at New Corrales, Agusan Canyon, Manolo Fortich, Bukidnon.");
+    });
+});
